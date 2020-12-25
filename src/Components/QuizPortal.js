c0bd84c44b20de235b8f4a9514aef634446460aa
R Narayan <nr.rnarayan@gmail.com>
2020-12-25 09:56:01 +0530

    er

--- src/Components/QuizPortal.js
+++ src/Components/QuizPortal.js
@@ -131,31 +131,31 @@ const Quiz = ({ auth }) => {
 		document.addEventListener('contextmenu', function (e) {
 			e.preventDefault();
 		});
-		// document.onkeydown = function (e) {
-		// 	if (e.keyCode == 123) {
-		// 		return false;
-		// 	}
-		// 	if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
-		// 		return false;
-		// 	}
-		// 	if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
-		// 		return false;
-		// 	}
-		// 	if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
-		// 		return false;
-		// 	}
-		// 	if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
-		// 		return false;
-		// 	}
-		// 	if (e.ctrlKey && e.shiftKey) {
-		// 		return false;
-		// 	}
-		// 	if (e.ctrlKey) {
-		// 		return false;
-		// 	}
-		// 	if (e.shiftKey) {
-		// 		return false;
-		// 	}
+		document.onkeydown = function (e) {
+			if (e.keyCode == 123) {
+				return false;
+			}
+			if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+				return false;
+			}
+			if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+				return false;
+			}
+			if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+				return false;
+			}
+			if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+				return false;
+			}
+			if (e.ctrlKey && e.shiftKey) {
+				return false;
+			}
+			if (e.ctrlKey) {
+				return false;
+			}
+			if (e.shiftKey) {
+				return false;
+			}
 		};
 		const uns = firebase.auth().onAuthStateChanged(async (user) => {
 			if (!user) {
