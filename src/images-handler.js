8ca163fae04c1490cbe2f6c4f7d9134fac6f8f98
R Narayan <nr.rnarayan@gmail.com>
2020-12-27 08:49:11 +0530

    Added new images to image-handler

--- src/images-handler.js
+++ src/images-handler.js
@@ -5,6 +5,18 @@ import img22e from './img/img22e.jpg';
 import img25e from './img/img25e.png';
 import img27l from './img/img27l.png';
 import img30e from './img/img30e.png';
+import img34e from './img/img34e.png';
+import img39e from './img/img39e.png';
+import img41l from './img/img41l.jpeg';
+import img42l from './img/img42l.jpeg';
+import img45l from './img/img45l.png';
+import img46m from './img/img46m.jpeg';
+import img54e from './img/img54e.png';
+import img54m from './img/img54m.jpeg';
+import img55m from './img/img55m.jpeg';
+import img56m from './img/img56m.jpeg';
+import img57m from './img/img57m.jpeg';
+import img58m from './img/img58m.jpeg';
 
 export default (img) => {
 	console.log(img);
@@ -15,4 +27,16 @@ export default (img) => {
 	if (img == 'img25e.png') return <img src={img25e} className='qimg' />;
 	if (img == 'img27l.png') return <img src={img27l} className='qimg' />;
 	if (img == 'img30e.png') return <img src={img30e} className='qimg' />;
+	if (img == 'img34e.png') return <img src={img34e} className='qimg' />;
+	if (img == 'img39e.png') return <img src={img39e} className='qimg' />;
+	if (img == 'img41l.jpeg') return <img src={img41l} className='qimg' />;
+	if (img == 'img42l.jpeg') return <img src={img42l} className='qimg' />;
+	if (img == 'img45l.png') return <img src={img45l} className='qimg' />;
+	if (img == 'img46m.jpeg') return <img src={img46m} className='qimg' />;
+	if (img == 'img54e.png') return <img src={img54e} className='qimg' />;
+	if (img == 'img54m.jpeg') return <img src={img54m} className='qimg' />;
+	if (img == 'img55m.jpeg') return <img src={img55m} className='qimg' />;
+	if (img == 'img56m.jpeg') return <img src={img56m} className='qimg' />;
+	if (img == 'img57m.jpeg') return <img src={img57m} className='qimg' />;
+	if (img == 'img58m.jpeg') return <img src={img58m} className='qimg' />;
 };
